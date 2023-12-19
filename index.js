c6f41274609f18ee1971f478128e0432ad7400c9
Lepurks <153022566+Lepurks@users.noreply.github.com>
2023-12-19 11:30:43 +0000

    ghj

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const PORT = 8080;
 // Listener für express-instanz
 app.listen(
     PORT,
-    () => console.log('server ready at http://localhost''${PORT}')
+    () => console.log('server ready at http://localhost"${PORT}')
 );
