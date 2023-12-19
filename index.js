e6727d5f352315b961d25d1fb6e5b330952f3bde
Lepurks <153022566+Lepurks@users.noreply.github.com>
2023-12-19 11:28:34 +0000

    hjkgh

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+// import
 
+const app = require('express')();
+const PORT = 8080;
+
+// Listener für express-instanz
+app.listen(
+    PORT,
+    () => console.log('server ready at http://localhost''${PORT}')
+);
